17ae348cfbf31b5ba09647a82548f3afac7152b6
hmc <2394002693@qq.com>
2019-06-28 16:26:51 +0800

    aaa

---
+++ ssd/infer1.py
@@ -0,0 +1,160 @@
+import os
+import time
+import numpy as np
+import argparse
+import functools
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import paddle
+import paddle.fluid as fluid
+import reader
+from mobilenet_ssd import build_mobilenet_ssd
+from utility import add_arguments, print_arguments
+
+parser = argparse.ArgumentParser(description=__doc__)
+add_arg = functools.partial(add_arguments, argparser=parser)
+# yapf: disable
+add_arg('dataset',          str,   'Mydata',    "coco and pascalvoc.")
+add_arg('use_gpu',          bool,  False,      "Whether use GPU.")
+add_arg('image_path',       str,   '',        "The image used to inference and visualize.")
+add_arg('model_dir',        str,   './model/best_model',     "The model path.")
+add_arg('nms_threshold',    float, 0.45,   "NMS threshold.")
+add_arg('confs_threshold',  float, 0.5,    "Confidence threshold to draw bbox.")
+add_arg('resize_h',         int,   300,    "The resized image height.")
+add_arg('resize_w',         int,   300,    "The resized image height.")
+add_arg('mean_value_B',     float, 127.5,  "Mean value for B channel which will be subtracted.")  #123.68
+add_arg('mean_value_G',     float, 127.5,  "Mean value for G channel which will be subtracted.")  #116.78
+add_arg('mean_value_R',     float, 127.5,  "Mean value for R channel which will be subtracted.")  #103.94
+# yapf: enable
+
+
+def infer(args, data_args, image_path, model_dir):
+    image_shape = [3, data_args.resize_h, data_args.resize_w]
+    num_classes = 7
+    label_list = data_args.label_list
+
+    image = fluid.layers.data(name='image', shape=image_shape, dtype='float32')
+    locs, confs, box, box_var = build_mobilenet_ssd(image, num_classes,
+                                                    image_shape)
+    nmsed_out = fluid.layers.detection_output(
+        locs, confs, box, box_var, nms_threshold=args.nms_threshold)
+
+    place = fluid.CUDAPlace(0) if args.use_gpu else fluid.CPUPlace()
+    exe = fluid.Executor(place)
+    # yapf: disable
+    if model_dir:
+        def if_exist(var):
+            return os.path.exists(os.path.join(model_dir, var.name))
+        fluid.io.load_vars(exe, model_dir, predicate=if_exist)
+    # yapf: enable
+    #infer_reader = reader.infer(data_args, image_path)
+    feeder = fluid.DataFeeder(place=place, feed_list=[image])
+
+    #data = infer_reader()
+
+    # switch network to test mode (i.e. batch norm test mode)
+    test_program = fluid.default_main_program().clone(for_test=True)
+    #print('################')
+    def work(image_path):
+        #nonlocal reader
+        nonlocal feeder
+        nonlocal test_program
+        nonlocal data_args
+        nonlocal nmsed_out
+        nonlocal exe
+        nonlocal args
+        nonlocal label_list
+
+        data=reader.infer(data_args,image_path)()
+        nmsed_out_v, = exe.run(test_program,
+                               feed=feeder.feed([[data]]),
+                               fetch_list=[nmsed_out],
+                               return_numpy=False)
+        nmsed_out_v = np.array(nmsed_out_v)
+       # draw_bounding_box_on_image(image_path, nmsed_out_v, args.confs_threshold, label_list)
+        return getList(image_path, nmsed_out_v,args.confs_threshold, label_list)
+    return work
+
+def getList(image_path, nms_out, confs_threshold, label_list):
+    image=Image.open(image_path)
+    im_width, im_height=image.size
+    rxmin,rymin,rxmax,rymax,rlabel=[],[],[],[],[]
+
+    for dt in nms_out:
+        if dt[1]<confs_threshold:
+            continue
+        rlabel.append(label_list[int(dt[0])])
+        
+        xmin,ymin,xmax,ymax=clip_bbox(dt[2:])
+        rxmin.append(xmin*im_width)
+        rymin.append(ymin*im_height)
+        rxmax.append(xmax*im_width)
+        rymax.append(ymax*im_height)
+    return rxmin,rymin,rxmax,rymax,rlabel
+
+
+def draw_bounding_box_on_image(image_path, nms_out, confs_threshold,
+                               label_list):
+    image = Image.open(image_path)
+    draw = ImageDraw.Draw(image)
+    im_width, im_height = image.size
+
+    for dt in nms_out:
+        if dt[1] < confs_threshold:
+            continue
+        category_id = dt[0]
+        bbox = dt[2:]
+        xmin, ymin, xmax, ymax = clip_bbox(dt[2:])
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                      ymin * im_height, ymax * im_height)
+        draw.line(
+            [(left, top), (left, bottom), (right, bottom), (right, top),
+             (left, top)],
+            width=4,
+            fill='green')
+        if image.mode == 'RGB':
+            draw.text((left, top), label_list[int(category_id)], (0, 255, 0))
+    image_name = "images/" + image_path.split('/')[-1]
+    print("image with bbox drawed saved as {}".format(image_name))
+    image.save(image_name)
+
+
+def clip_bbox(bbox):
+    xmin = max(min(bbox[0], 1.), 0.)
+    ymin = max(min(bbox[1], 1.), 0.)
+    xmax = max(min(bbox[2], 1.), 0.)
+    ymax = max(min(bbox[3], 1.), 0.)
+    return xmin, ymin, xmax, ymax
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print_arguments(args)
+
+    data_dir = 'data/Mydata'
+    label_file = 'label_list'
+
+    data_args = reader.Settings(
+        dataset=args.dataset,
+        data_dir=data_dir,
+        label_file=label_file,
+        resize_h=args.resize_h,
+        resize_w=args.resize_w,
+        mean_value=[args.mean_value_B, args.mean_value_G, args.mean_value_R],
+        apply_distort=False,
+        apply_expand=False,
+        ap_version='')
+    f=infer(
+        args,
+        data_args=data_args,
+        image_path=args.image_path,
+        model_dir=args.model_dir)
+    while True:
+        image_path=input('input the path of image\n')
+        if image_path in 'quitexit':
+            break
+        a,b,c,d,e=f(image_path)
+        for i in range(len(a)):
+            print(a[i],b[i],c[i],d[i],e[i])
---
+++ ssd/reader.py
@@ -0,0 +1,268 @@
+# Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import xml.etree.ElementTree
+import os
+import time
+import copy
+import six
+import math
+import numpy as np
+from PIL import Image
+from PIL import ImageDraw
+import image_util
+import paddle
+
+
+class Settings(object):
+    def __init__(self,
+                 dataset=None,
+                 data_dir=None,
+                 label_file=None,
+                 resize_h=300,
+                 resize_w=300,
+                 mean_value=[127.5, 127.5, 127.5],
+                 apply_distort=True,
+                 apply_expand=True,
+                 ap_version='11point'):
+        self._dataset = dataset
+        self._ap_version = ap_version
+        self._data_dir = data_dir
+        self._label_list = []
+        label_fpath = os.path.join(data_dir, label_file)
+        for line in open(label_fpath):
+            self._label_list.append(line.strip())
+
+        self._apply_distort = apply_distort
+        self._apply_expand = apply_expand
+        self._resize_height = resize_h
+        self._resize_width = resize_w
+        self._img_mean = np.array(mean_value)[:, np.newaxis, np.newaxis].astype(
+            'float32')
+        self._expand_prob = 0.5
+        self._expand_max_ratio = 4
+        self._hue_prob = 0.5
+        self._hue_delta = 18
+        self._contrast_prob = 0.5
+        self._contrast_delta = 0.5
+        self._saturation_prob = 0.5
+        self._saturation_delta = 0.5
+        self._brightness_prob = 0.5
+        self._brightness_delta = 0.125
+
+    @property
+    def dataset(self):
+        return self._dataset
+
+    @property
+    def ap_version(self):
+        return self._ap_version
+
+    @property
+    def apply_distort(self):
+        return self._apply_expand
+
+    @property
+    def apply_distort(self):
+        return self._apply_distort
+
+    @property
+    def data_dir(self):
+        return self._data_dir
+
+    @data_dir.setter
+    def data_dir(self, data_dir):
+        self._data_dir = data_dir
+
+    @property
+    def label_list(self):
+        return self._label_list
+
+    @property
+    def resize_h(self):
+        return self._resize_height
+
+    @property
+    def resize_w(self):
+        return self._resize_width
+
+    @property
+    def img_mean(self):
+        return self._img_mean
+
+
+def preprocess(img, bbox_labels, mode, settings):
+    img_width, img_height = img.size
+    sampled_labels = bbox_labels
+    if mode == 'train':
+        if settings._apply_distort:
+            img = image_util.distort_image(img, settings)
+        if settings._apply_expand:
+            img, bbox_labels, img_width, img_height = image_util.expand_image(
+                img, bbox_labels, img_width, img_height, settings)
+        # sampling
+        batch_sampler = []
+        # hard-code here
+        batch_sampler.append(
+            image_util.sampler(1, 1, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0))
+        batch_sampler.append(
+            image_util.sampler(1, 50, 0.3, 1.0, 0.5, 2.0, 0.1, 0.0))
+        batch_sampler.append(
+            image_util.sampler(1, 50, 0.3, 1.0, 0.5, 2.0, 0.3, 0.0))
+        batch_sampler.append(
+            image_util.sampler(1, 50, 0.3, 1.0, 0.5, 2.0, 0.5, 0.0))
+        batch_sampler.append(
+            image_util.sampler(1, 50, 0.3, 1.0, 0.5, 2.0, 0.7, 0.0))
+        batch_sampler.append(
+            image_util.sampler(1, 50, 0.3, 1.0, 0.5, 2.0, 0.9, 0.0))
+        batch_sampler.append(
+            image_util.sampler(1, 50, 0.3, 1.0, 0.5, 2.0, 0.0, 1.0))
+        sampled_bbox = image_util.generate_batch_samples(batch_sampler,
+                                                         bbox_labels)
+
+        img = np.array(img)
+        if len(sampled_bbox) > 0:
+            idx = int(np.random.uniform(0, len(sampled_bbox)))
+            img, sampled_labels = image_util.crop_image(
+                img, bbox_labels, sampled_bbox[idx], img_width, img_height)
+
+        img = Image.fromarray(img)
+    img = img.resize((settings.resize_w, settings.resize_h), Image.ANTIALIAS)
+    img = np.array(img)
+
+    if mode == 'train':
+        mirror = int(np.random.uniform(0, 2))
+        if mirror == 1:
+            img = img[:, ::-1, :]
+            for i in six.moves.xrange(len(sampled_labels)):
+                tmp = sampled_labels[i][1]
+                sampled_labels[i][1] = 1 - sampled_labels[i][3]
+                sampled_labels[i][3] = 1 - tmp
+    # HWC to CHW
+    if len(img.shape) == 3:
+        img = np.swapaxes(img, 1, 2)
+        img = np.swapaxes(img, 1, 0)
+    # RBG to BGR
+    img = img[[2, 1, 0], :, :]
+    img = img.astype('float32')
+    img -= settings.img_mean
+    img = img * 0.007843
+    return img, sampled_labels
+
+
+def Mydata(settings, file_list, mode, batch_size, shuffle):
+    def reader():
+        if mode == 'train' and shuffle:
+            np.random.shuffle(file_list)
+        batch_out = []
+        cnt = 0
+        for image in file_list:
+            image_path, label_path = image.split()
+            image_path = os.path.join(settings.data_dir, image_path)
+            label_path = os.path.join(settings.data_dir, label_path)
+            if not os.path.exists(image_path):
+                raise ValueError("%s is not exist, you should specify "
+                                 "data path correctly." % image_path)
+            im = Image.open(image_path)
+            if im.mode == 'L':
+                im = im.convert('RGB')
+            im_width, im_height = im.size
+
+            # layout: label | xmin | ymin | xmax | ymax | difficult
+            bbox_labels = []
+            root = xml.etree.ElementTree.parse(label_path).getroot()
+            for object in root.findall('object'):
+                bbox_sample = []
+                # start from 1
+                bbox_sample.append(
+                    float(settings.label_list.index(object.find('name').text)))
+                bbox = object.find('bndbox')
+                try:
+                    difficult = float(object.find('difficult').text)
+                except:
+                    difficult = float(object.find('Difficult').text)
+                bbox_sample.append(float(bbox.find('xmin').text) / im_width)
+                bbox_sample.append(float(bbox.find('ymin').text) / im_height)
+                bbox_sample.append(float(bbox.find('xmax').text) / im_width)
+                bbox_sample.append(float(bbox.find('ymax').text) / im_height)
+                bbox_sample.append(difficult)
+                bbox_labels.append(bbox_sample)
+            im, sample_labels = preprocess(im, bbox_labels, mode, settings)
+            sample_labels = np.array(sample_labels)
+            if len(sample_labels) == 0: continue
+            im = im.astype('float32')
+            boxes = sample_labels[:, 1:5]
+            lbls = sample_labels[:, 0].astype('int32')
+            difficults = sample_labels[:, -1].astype('int32')
+
+            batch_out.append((im, boxes, lbls, difficults))
+            if len(batch_out) == batch_size:
+                yield batch_out
+                cnt += len(batch_out)
+                batch_out = []
+
+        if mode == 'test' and len(batch_out) > 1:
+            yield batch_out
+            cnt += len(batch_out)
+            batch_out = []
+
+    return reader
+
+def train(settings,
+          file_list,
+          batch_size,
+          shuffle=True,
+          num_workers=8,
+          enable_ce=False):
+    file_path = os.path.join(settings.data_dir, file_list)
+    readers = []
+    images = [line.strip() for line in open(file_path)]
+    np.random.shuffle(images)
+    n = int(math.ceil(len(images) // num_workers))
+    image_lists = [images[i:i + n] for i in range(0, len(images), n)]
+    for l in image_lists:
+        readers.append(Mydata(settings, l, 'train', batch_size, shuffle))
+    return paddle.reader.multiprocess_reader(readers, False)
+
+
+def test(settings, file_list, batch_size):
+    file_list = os.path.join(settings.data_dir, file_list)
+    image_list = [line.strip() for line in open(file_list)]
+    return Mydata(settings, image_list, 'test', batch_size, False)
+
+
+def infer(settings, image_path):
+    def reader():
+        if not os.path.exists(image_path):
+            raise ValueError("%s is not exist, you should specify "
+                             "data path correctly." % image_path)
+        img = Image.open(image_path)
+        if img.mode == 'L':
+            img = img.convert('RGB')
+        im_width, im_height = img.size
+        img = img.resize((settings.resize_w, settings.resize_h),
+                         Image.ANTIALIAS)
+        img = np.array(img)
+        # HWC to CHW
+        if len(img.shape) == 3:
+            img = np.swapaxes(img, 1, 2)
+            img = np.swapaxes(img, 1, 0)
+        # RBG to BGR
+        img = img[[2, 1, 0], :, :]
+        img = img.astype('float32')
+        img -= settings.img_mean
+        img = img * 0.007843
+        return img
+
+    return reader
