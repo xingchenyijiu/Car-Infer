2e43a705f880ce945717039b553c0fe25e2e49a8
hmc <2394002693@qq.com>
2019-07-04 10:00:56 +0800

    update

--- ssd/cs.cpp
+++ ssd/cs.cpp
@@ -1,6 +1,7 @@
 #include "paddle_inference_api.h"
 #include <vector>
 #include <string>
+namespace paddle {
 void createConfig(NativeConfig *config,const std::string& model_dirname) {
 	config->use_gpu=false;
 	config->SetCpuMathLibraryNumThreads(1);
@@ -20,7 +21,7 @@ void run(int batch_size,const std::string& model_dirname) {
 	int c=3,h=300,w=300;
 	float *data=new float[batch_size*c*w*h];
 
-	PaddleTensor=tensor;
+	PaddleTensor tensor;
 	tensor.name="image";
 	tensor.shape=std::vector<int>({batch_size,c,h,w});
 	tensor.data=PaddleBuf(static_cast<void*>(data),sizeof(float)*(batch_size*c*h*w));
@@ -30,10 +31,10 @@ void run(int batch_size,const std::string& model_dirname) {
 	std::vector<PaddleTensor> output;
 	predictor->Run(ptf,&output,batch_size);
 
-	const size_t num_elements=oupput.front().data.length()/(sizeof(float));
+	const size_t num_elements=output.front().data.length()/(sizeof(float));
 	auto *data_out=static_cast<float *> (output.front().data.data());
 }
-
+}
 int main() {
-	run(1,"./cppmodel/");
+	paddle::run(1,"./cppmodel/");
 }
